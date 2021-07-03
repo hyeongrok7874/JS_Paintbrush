1b346c6ef965a0877d385561e2eba0c5098472b7
hyeongrok7874 <hyeongrok7874@naver.com>
2021-07-03 16:44:37 +0900

    Add link

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 JS로 그림판을 만들어 보자!!
 
 (기능반 과제인건 비밀)
+
+[그림판](https://hyeongrok7874.github.io/JS_Paintbrush/)
--- script/scriptl.js
+++ script/scriptl.js
@@ -1,9 +1,64 @@
-let canvas = document.getElementById('canvas');
-draw(canvas);
+let pos = {
+    drawable: false,
+    X: -1,
+    Y: -1
+}
 
-function draw(canvas) {
-    let ctx = canvas.getContext("2d");
-    ctx.moveTo(50, 100);
-    ctx.lineTo(100, 100);
+let canvas, ctx;
+
+window.onload = function () {
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+
+    canvas.addEventListener("mousedown", listener);
+    canvas.addEventListener("mousemove", listener);
+    canvas.addEventListener("mouseup", listener);
+    canvas.addEventListener("mouseout", listener);
+}
+
+function listener(event) {
+    switch (event.type) {
+        case "mousedown":
+            initDraw(event);
+            console.log("success");
+            break;
+        case "mousemove":
+            if (pos.drawable)
+                draw(event);
+            break;
+        case "mouseout":
+        case "mouseup":
+            finishDraw();
+            break;
+    }
+}
+function initDraw(event) {
+    console.log("initDraw")
+    ctx.fillstyle = "black";
+    ctx.beginPath();
+    pos.drawable = true;
+    let coors = getPosition(event);
+    pos.X = coors.X;
+    pos.Y = coors.Y;
+    ctx.moveTo(pos.X, pos.Y);
+}
+
+function draw(event) {
+    let coors = getPosition(event);
+    ctx.lineTo(coors.X, coors.Y);
+    pos.X = coors.X;
+    pos.Y = coors.Y;
     ctx.stroke();
+}
+
+function finishDraw() {
+    pos.drawable = false;
+    pos.X = -1;
+    pos.Y = -1;
+}
+
+function getPosition(event) {
+    let x = event.pageX - canvas.offsetLeft;
+    let y = event.pageY - canvas.offsetTop;
+    return { X: x, Y: y };
 }
