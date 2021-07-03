5924d28def9bdc6aba1de97b22ea3725fe1ca723
hyeongrok7874 <hyeongrok7874@naver.com>
2021-07-03 16:43:54 +0900

    Add link

--- script/scriptl.js
+++ script/scriptl.js
@@ -1,1 +1,9 @@
-let
+let canvas = document.getElementById('canvas');
+draw(canvas);
+
+function draw(canvas) {
+    let ctx = canvas.getContext("2d");
+    ctx.moveTo(50, 100);
+    ctx.lineTo(100, 100);
+    ctx.stroke();
+}
