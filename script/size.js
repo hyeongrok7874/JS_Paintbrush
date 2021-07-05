32ba34aa69a21ffefd12188463911db88fb37209
hyeongrok7874 <hyeongrok7874@naver.com>
2021-07-05 23:16:21 +0900

    Add New functions

--- script/size.js
+++ script/size.js
@@ -1,10 +1,26 @@
 let size = document.getElementById("size");
 let plus = document.getElementById("plus");
 let minus = document.getElementById("minus");
+let pencil = document.getElementById("pencil");
+let brush = document.getElementById("brush");
+let eraser = document.getElementById("eraser");
 
-plus.onclick = function(){
+plus.onclick = function () {
+    console.log("plus")
     size.innerHTML = parseInt(size.innerHTML) + 1;
 }
-minus.onclick = function(){
+minus.onclick = function () {
+    if (size.innerHTML == '0') {
+        return;
+    }
     size.innerHTML = parseInt(size.innerHTML) - 1;
+}
+pencil.onclick = function () {
+    size.innerHTML = 3;
+}
+brush.onclick = function () {
+    size.innerHTML = 7;
+}
+eraser.onclick = function () {
+    size.innerHTML = 1;
 }
