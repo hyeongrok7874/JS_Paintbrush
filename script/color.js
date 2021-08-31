5f2b33dda1fd0498514028d4ccc0ad4b015e730d
hyeongrok7874 <hyeongrok7874@naver.com>
2021-08-31 10:10:41 +0900

    Code Refactoring

--- script/color.js
+++ script/color.js
@@ -6,92 +6,42 @@ let green = document.getElementById("green");
 let black = document.getElementById("black");
 let purple = document.getElementById("purple");
 let pink = document.getElementById("pink");
+let color = document.getElementsByClassName("color");
 
-red.onclick = function () {
+color[0].onclick = function () {
     context.strokeStyle = 'red';
-    red.style.border = "3px solid #000000";
-    blue.style.border = "3px solid #FFFFFF"
-    yellow.style.border = "3px solid #FFFFFF"
-    orange.style.border = "3px solid #FFFFFF"
-    green.style.border = "3px solid #FFFFFF"
-    black.style.border = "3px solid #FFFFFF"
-    purple.style.border = "3px solid #FFFFFF"
-    pink.style.border = "3px solid #FFFFFF"
 }
 blue.onclick = function () {
     context.strokeStyle = 'blue';
-    red.style.border = "3px solid #FFFFFF";
-    blue.style.border = "3px solid #000000"
-    yellow.style.border = "3px solid #FFFFFF"
-    orange.style.border = "3px solid #FFFFFF"
-    green.style.border = "3px solid #FFFFFF"
-    black.style.border = "3px solid #FFFFFF"
-    purple.style.border = "3px solid #FFFFFF"
-    pink.style.border = "3px solid #FFFFFF"
 }
 yellow.onclick = function () {
     context.strokeStyle = 'yellow';
-    red.style.border = "3px solid #FFFFFF";
-    blue.style.border = "3px solid #FFFFFF"
-    yellow.style.border = "3px solid #000000"
-    orange.style.border = "3px solid #FFFFFF"
-    green.style.border = "3px solid #FFFFFF"
-    black.style.border = "3px solid #FFFFFF"
-    purple.style.border = "3px solid #FFFFFF"
-    pink.style.border = "3px solid #FFFFFF"
 }
 orange.onclick = function () {
     context.strokeStyle = 'orange';
-    red.style.border = "3px solid #FFFFFF";
-    blue.style.border = "3px solid #FFFFFF"
-    yellow.style.border = "3px solid #FFFFFF"
-    orange.style.border = "3px solid #000000"
-    green.style.border = "3px solid #FFFFFF"
-    black.style.border = "3px solid #FFFFFF"
-    purple.style.border = "3px solid #FFFFFF"
-    pink.style.border = "3px solid #FFFFFF"
 }
 green.onclick = function () {
     context.strokeStyle = 'green';
-    red.style.border = "3px solid #FFFFFF";
-    blue.style.border = "3px solid #FFFFFF"
-    yellow.style.border = "3px solid #FFFFFF"
-    orange.style.border = "3px solid #FFFFFF"
-    green.style.border = "3px solid #000000"
-    black.style.border = "3px solid #FFFFFF"
-    purple.style.border = "3px solid #FFFFFF"
-    pink.style.border = "3px solid #FFFFFF"
 }
 black.onclick = function () {
     context.strokeStyle = 'black';
-    red.style.border = "3px solid #FFFFFF";
-    blue.style.border = "3px solid #FFFFFF"
-    yellow.style.border = "3px solid #FFFFFF"
-    orange.style.border = "3px solid #FFFFFF"
-    green.style.border = "3px solid #FFFFFF"
-    black.style.border = "3px solid #000000"
-    purple.style.border = "3px solid #FFFFFF"
-    pink.style.border = "3px solid #FFFFFF"
 }
 purple.onclick = function () {
     context.strokeStyle = 'purple';
-    red.style.border = "3px solid #FFFFFF";
-    blue.style.border = "3px solid #FFFFFF"
-    yellow.style.border = "3px solid #FFFFFF"
-    orange.style.border = "3px solid #FFFFFF"
-    green.style.border = "3px solid #FFFFFF"
-    black.style.border = "3px solid #FFFFFF"
-    purple.style.border = "3px solid #000000"
-    pink.style.border = "3px solid #FFFFFF"
 }
 pink.onclick = function () {
     context.strokeStyle = 'pink';
-    red.style.border = "3px solid #FFFFFF";
-    blue.style.border = "3px solid #FFFFFF"
-    yellow.style.border = "3px solid #FFFFFF"
-    orange.style.border = "3px solid #FFFFFF"
-    green.style.border = "3px solid #FFFFFF"
-    black.style.border = "3px solid #FFFFFF"
-    purple.style.border = "3px solid #FFFFFF"
-    pink.style.border = "3px solid #000000"
+}
+
+for (let i = 0; i < 8; i++){
+    color[i].onclick = function () {
+        for (let j = 0; j < 8; j++){
+            if (j == i) {
+                color[j].style.border = "3px solid #000000"
+            }
+            else {
+                color[j].style.border = "3px solid #FFFFFF"
+            }
+        }
+    }
 }
